c5b75886f78dd9574a94f7ee27cdf94c1afa8f10
Pieter-Jan Briers <pieterjan.briers@gmail.com>
2017-10-30 01:57:17 +0100

    Extra unit test.
    It generates 1 kb of noise and diffs that. Also made the unit tests
    parallize

--- SS14.Shared.Bsdiff.UnitTesting/TestClass.cs
+++ SS14.Shared.Bsdiff.UnitTesting/TestClass.cs
@@ -1,9 +1,12 @@
 ﻿using NUnit.Framework;
 using System.Linq;
+using System;
 
 namespace SS14.Shared.Bsdiff.UnitTesting
 {
     [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    [TestOf(typeof(Bsdiff))]
     public class TestClass
     {
         [Test]
@@ -11,6 +14,31 @@ namespace SS14.Shared.Bsdiff.UnitTesting
         {
             var bytes = new byte[] { 0, 1, 2, 3, 5 };
             var bytes2 = new byte[] { 0, 1, 2, 3, 10 };
+
+            var patch = Bsdiff.GenerateBzip2Diff(bytes, bytes2);
+            var result = Bsdiff.ApplyBzip2Patch(bytes, patch);
+
+            bool equals = result.OrderBy(a => a).SequenceEqual(bytes2.OrderBy(a => a));
+            Assert.True(equals);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void TestNoise()
+        {
+            var random = new Random();
+            var bytes = new byte[1024];
+            random.NextBytes(bytes);
+            var bytes2 = new byte[1024];
+            bytes.CopyTo(bytes2, 0);
+
+            for (var i = 0; i < bytes2.Length; i++)
+            {
+                if (random.NextDouble() > 0.5)
+                {
+                    bytes2[i] = (byte)random.Next();
+                }
+            }
 
             var patch = Bsdiff.GenerateBzip2Diff(bytes, bytes2);
             var result = Bsdiff.ApplyBzip2Patch(bytes, patch);
